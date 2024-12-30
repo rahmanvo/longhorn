5c0cec02b06e1d47b29c0ce52281830be2c256f4
rahmanvo <163956578+rahmanvo@users.noreply.github.com>
2024-12-30 01:12:46 +0000

    add install-longhorn.sh

---
+++ examples/install-longhorn.sh
@@ -0,0 +1,4 @@
+helm repo add longhorn https://charts.longhorn.io
+helm repo update
+helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace --version 1.5.3
+kubectl -n longhorn-system get pod
